449bf9bcea0e9d3a9eaee8e0160448d63405a48d
Wes Hediger <wes.hediger@gmail.com>
2016-09-18 17:55:32 -0600

    add contact info

--- js/main.js
+++ js/main.js
@@ -1,7 +1,15 @@
 $(document).ready(function(){
+
+
+  var bodyWidth = $('body').width()/100 * 60;
+  $('.title').delay(1000).animate({height:'230px', width: '280px', marginLeft: bodyWidth + "px"}, 2000, 'swing');
+
   new Vivus('logo', {
     duration: 400,
     file: 'wah-blue.svg',
     type: 'oneByOne',
     pathTimingFunction: Vivus.EASE_OUT});
-});
+  });
+
+  $('#name').delay(7000).fadeIn(500, 'swing');
+  $('#roll').delay(7500).fadeIn(500, 'swing');
