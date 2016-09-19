3dfffe54d2c96ab684c56e7724602b99ffeed575
Wes Hediger <wes.hediger@gmail.com>
2016-09-18 18:02:45 -0600

    add animation to info

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
 
-
+  $('.info').hide();
   var bodyWidth = $('body').width()/100 * 60;
   $('.title').delay(1000).animate({height:'230px', width: '280px', marginLeft: bodyWidth + "px"}, 2000, 'swing');
 
@@ -13,3 +13,6 @@ $(document).ready(function(){
 
   $('#name').delay(7000).fadeIn(500, 'swing');
   $('#roll').delay(7500).fadeIn(500, 'swing');
+  $('.phone').delay(8000).fadeIn(500, 'swing');
+  $('.github').delay(8300).fadeIn(500, 'swing');
+  $('.linkedin').delay(8400).fadeIn(500, 'swing');
