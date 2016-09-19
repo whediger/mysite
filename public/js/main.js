95964917474e9073ea30b68c92876de6e93ff92f
Wes Hediger <wes.hediger@gmail.com>
2016-09-18 19:25:02 -0600

    rm meyer reset cdn, add as file

---
+++ public/js/main.js
@@ -0,0 +1,28 @@
+$(document).ready(function(){
+  $('.title').hide();
+  $('.info').hide();
+  var bodyWidth = $('body').width()/100 * 60;
+  $('.title').delay(1000)
+  .fadeIn(200)
+  .animate({
+    height:'230px',
+    width: '280px',
+    marginLeft: bodyWidth + "px",
+    marginTop: '225px'},
+      8000, 'swing',
+    //callback: draw logo after animation finishes
+    function(){
+      new Vivus('logo', {
+        duration: 400,
+        file: 'wah-blue.svg',
+        type: 'oneByOne',
+        pathTimingFunction: Vivus.EASE_OUT});
+  });
+
+  $('#name').delay(16000).fadeIn(500, 'swing');
+  $('#roll').delay(16500).fadeIn(500, 'swing');
+  $('.phone').delay(17000).fadeIn(500, 'swing');
+  $('.github').delay(17300).fadeIn(500, 'swing');
+  $('.linkedin').delay(17400).fadeIn(500, 'swing');
+
+});
